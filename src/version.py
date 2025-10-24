0455f364427b29b0cbfd6824eded4187cc6cca16
trickerer01 <onlysuffering+1@gmail.com>
2025-10-24 22:43:30 +0700

    Convert excessive print statements to logging. Clean input.py.
    (cherry picked from RV commit 49cb86ce826b5943c9378aed0785e9bc494fd210)

--- src/version.py
+++ src/version.py
@@ -9,7 +9,7 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 APP_NAME = 'RG'
 APP_VER_MAJOR = '1'
 APP_VER_SUB = '8'
-APP_REVISION = '137'
+APP_REVISION = '138'
 APP_VERSION = f'{APP_VER_MAJOR}.{APP_VER_SUB}.{APP_REVISION}'
 
 #
